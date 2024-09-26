1bef6fb9ec48ddad186815293d26b3144df240bc
FancyCodeMaster <fancycodemaster@gmail.com>
2024-09-26 13:02:43 +0545

    checking experiment tracker

--- pipeline_config.py
+++ pipeline_config.py
@@ -1,4 +1,4 @@
-ROOT_PATH = '/media/fancycodemaster/b974d212-7880-485c-a6d5-3590d14a02b1/Items/Personal Projects/restart/regression'
+ROOT_PATH = './'
 
 datasetfolder = 'dataset'
 csvfilename = 'Student_Performance.csv'
@@ -6,4 +6,4 @@ dependent_variable = 'Performance Index'
 test_size = 0.2
 min_accuracy = 0.9
 
-model_save_path = '/media/fancycodemaster/b974d212-7880-485c-a6d5-3590d14a02b1/Items/Personal Projects/restart/regression/saved_model/stdperf.pkl'
+model_save_path = './saved_model/stdperf.pkl'
